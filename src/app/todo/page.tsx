aef12d549fd3c67b1018c3c567f774350ab7a0c2
double <double.jys@gmail.com>
2025-04-16 15:10:04 +0800

    change text color

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -116,7 +116,7 @@ export default function TodoPage() {
             value={newTodoText}
             onChange={(e) => setNewTodoText(e.target.value)}
             placeholder="What needs to be done?"
-            className="flex-1 px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="flex-1 px-4 py-2 border text-white rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
           <button
             type="submit"
